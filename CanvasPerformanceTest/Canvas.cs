4d3c21b80bea51fe328f1d9a4d815998be8fc6e8
Keito <97142968+techo-pen@users.noreply.github.com>
2023-02-15 07:48:27 +0900

    WIP:サイズ変更されないと表示がされない。

---
+++ CanvasPerformanceTest/Canvas.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows;
+using System.Windows.Media;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace CanvasPerformanceTest
+{
+    class Canvas: FrameworkElement
+    {
+        public static Point GetLocation(DependencyObject obj)
+        {
+            return (Point)obj.GetValue(LocationProperty);
+        }
+
+        public static void SetLocation(DependencyObject obj, Point value)
+        {
+            obj.SetValue(LocationProperty, value);
+        }
+
+        public static readonly DependencyProperty LocationProperty =
+            DependencyProperty.RegisterAttached("Location", typeof(Point), typeof(Canvas),
+            new FrameworkPropertyMetadata(default(Point), FrameworkPropertyMetadataOptions.AffectsArrange));
+
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
+        public ObservableCollection<UIElement> Children { get; private set; }
+
+        public Canvas()
+        {
+            Children = new ObservableCollection<UIElement>();
+
+            Children.CollectionChanged += Children_CollectionChanged;
+        }
+
+        void Children_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            if (e.OldItems != null)
+            {
+                foreach (UIElement oldItem in e.OldItems)
+                {
+                    RemoveVisualChild(oldItem);
+                }
+            }
+            if (e.NewItems != null)
+            {
+                foreach (UIElement newItem in e.NewItems)
+                {
+                    AddVisualChild(newItem);
+                }
+            }
+        }
+
+        protected override int VisualChildrenCount
+        {
+            get
+            {
+                return Children.Count;
+            }
+        }
+
+        protected override Visual GetVisualChild(int index)
+        {
+            return Children[index];
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            foreach (var child in Children)
+            {
+                var location = GetLocation(child);
+
+                child.Arrange(new Rect(location, child.DesiredSize));
+            }
+
+            return base.ArrangeOverride(finalSize);
+        }
+
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            foreach (var child in Children)
+            {
+                var fe = child as FrameworkElement;
+
+                if (fe != null)
+                {
+                    child.Measure(new Size(fe.Width, fe.Height));
+                }
+            }
+
+            return base.MeasureOverride(availableSize);
+        }
+    }
+}
---
+++ CanvasPerformanceTest/MainWindow.xaml.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CanvasPerformanceTest
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 備え付けコントロールで描画
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            canvas.Children.Clear();
+
+            Rectangle rect = new Rectangle();
+            rect.Width = 350;
+            rect.Height = 150;
+            rect.Stroke = new SolidColorBrush(Colors.Black);
+            rect.StrokeThickness = 3;
+
+            System.Windows.Controls.Canvas.SetLeft(rect,25);
+            System.Windows.Controls.Canvas.SetTop(rect, 25);
+            canvas.Children.Add(rect);
+
+            Random random = new Random();
+            List<Ellipse> ellipseList = new List<Ellipse>();
+            for (int i = 0; i < 10000; i++)
+            {
+                Ellipse elipse = new Ellipse();
+                elipse.Width = 2;
+                elipse.Height = 2;
+                elipse.Stroke = new SolidColorBrush(Colors.Black);
+                elipse.StrokeThickness = 2;
+                
+                int x = random.Next(0, 350);
+                int y = random.Next(0, 150);
+                System.Windows.Controls.Canvas.SetLeft(elipse, 25 + x);
+                System.Windows.Controls.Canvas.SetTop(elipse, 25 + y);
+                ellipseList.Add(elipse);
+            }
+
+            DateTime startTime = DateTime.Now;
+
+            foreach (var elipse in ellipseList)
+            {
+                canvas.Children.Add(elipse);
+            }
+
+            DateTime endTime = DateTime.Now;
+
+            normalTime.Text = (endTime - startTime).TotalSeconds.ToString();
+        }
+
+        private void improveDraw_Click(object sender, RoutedEventArgs e)
+        {
+            inproveCanvas.Children.Clear();
+
+            Rectangle rect = new Rectangle();
+            rect.Width = 350;
+            rect.Height = 150;
+            rect.Stroke = new SolidColorBrush(Colors.Black);
+            rect.StrokeThickness = 3;
+
+            CanvasPerformanceTest.Canvas.SetLocation(rect, new Point(0, 0));
+            inproveCanvas.Children.Add(rect);
+
+            Random random = new Random();
+            List<Ellipse> ellipseList = new List<Ellipse>();
+            for (int i = 0; i < 10000; i++)
+            {
+                Ellipse elipse = new Ellipse();
+                elipse.Width = 2;
+                elipse.Height = 2;
+                elipse.Stroke = new SolidColorBrush(Colors.Black);
+                elipse.StrokeThickness = 2;
+
+                int x = random.Next(0, 350);
+                int y = random.Next(0, 150);
+                CanvasPerformanceTest.Canvas.SetLocation(elipse, new Point(x, y));
+                ellipseList.Add(elipse);
+            }
+
+            DateTime startTime = DateTime.Now;
+
+            foreach (var elipse in ellipseList)
+            {
+                inproveCanvas.Children.Add(elipse);
+            }
+
+            DateTime endTime = DateTime.Now;
+
+            inprovelTime.Text = (endTime - startTime).TotalSeconds.ToString();
+            inproveCanvas.BringIntoView();
+        }
+    }
+}
